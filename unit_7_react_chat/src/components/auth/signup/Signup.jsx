d6822abef2b0a1fc38c0273112f27826066cb4ff
Keith David <kdavid8@student.cccs.edu>
2023-10-28 18:59:58 -0600

    login/signup round 2

--- unit_7_react_chat/src/components/auth/signup/Signup.jsx
+++ unit_7_react_chat/src/components/auth/signup/Signup.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 function Signup(props) {
-    const navigate = useNavigate();
+    const navigate = useNavigate('/chat');
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
