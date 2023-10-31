c4a0d5cfb12dc06cff9fde91f39a4d3b095becb0
BlakeLindsay <104230616+BlakeLindsay@users.noreply.github.com>
2023-10-30 19:39:28 -0600

    working getRoomList

--- unit_7_react_chat/src/components/chat/Display.jsx
+++ unit_7_react_chat/src/components/chat/Display.jsx
@@ -1,13 +1,17 @@
 import AddRoom from "./AddRoom";
 import Delete from "./Delete";
 import Update from "./Update";
+import { useState, useEffect } from 'react';
 
 function Display(props) {
+	let [roomList, setRoomList] = useState([]);
+	useEffect(() => {getRoomList()},[]);
+
 	return (
 		<div>
 			<span>
 				<div>Available Rooms</div>
-				<div></div>
+				<div>{roomListDiv(roomList)}</div>
 				<AddRoom />
 			</span>
 			<span>
@@ -25,23 +29,32 @@ function Display(props) {
 		</div>
 	)
 
+	// fetches and populates room list
 	async function getRoomList(){
 		try {
 			let res = await fetch('http://127.0.0.1:4000/room/list', {
 				headers: new Headers({
 					'content-type': 'application/json'
 				}),
-				method: 'GET',
-				body: JSON.stringify({
-					
-				})
+				method: 'GET'
 			});
 			let results = await res.json();
-			console.log(res);
+			console.log(results);
+			setRoomList(results.getAllRooms);
 		} catch(error) {
 			console.log(error);
 		}
 	}
+};
+
+function roomListDiv(roomList) {
+	return (
+		roomList.map((room, index) =>
+						<div key={index}>
+							{room.title}
+						</div>
+		)
+	)
 };
 
 export default Display;
